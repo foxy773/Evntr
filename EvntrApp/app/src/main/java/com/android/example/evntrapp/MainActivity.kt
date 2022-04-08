b67711f2c241c408c4d6fa7ba880c23422ed1778
samakab99 <samakab4@gmail.com>
2022-04-08 13:29:14 +0200

    splash screen, bottom nav meny

--- EvntrApp/app/src/main/java/com/android/example/evntrapp/MainActivity.kt
+++ EvntrApp/app/src/main/java/com/android/example/evntrapp/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.android.example.evntrapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        println("Hello World")
+        supportActionBar?.hide()
+        Handler().postDelayed({
+            val intent = Intent (this@MainActivity,MainActivity2::class.java)
+            startActivity(intent)
+        }, 3000)
     }
 }
---
+++ EvntrApp/app/src/main/java/com/android/example/evntrapp/MainActivity2.kt
@@ -0,0 +1,14 @@
+package com.android.example.evntrapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+
+
+    }
+}
